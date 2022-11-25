8b9d51c92d7c436ac7a010208792542534ce852e
Abel Manjon <abel.manjon@gmail.com>
2022-11-25 13:04:55 +0100

    cambios

--- midTerm.js
+++ midTerm.js
@@ -1,3 +1,4 @@
+// funcion para formulario
 const addPost = () => {
     let name = document.getElementById("name").value; 
     let email = document.getElementById("email").value;
@@ -21,3 +22,33 @@ window.addEventListener("load", () => {document.getElementById("btn-submit").add
     addPost();
    })})
 
+// funcion para traer data al index.html
+
+const getExternalApiInfo = async () => {
+   await fetch("https://jsonplaceholder.typicode.com/posts/?_limit=3")
+   .then((response) => response.json())
+   .then((posts) => {
+      let externalApiInfo = "";
+      posts.forEach((post) => {
+         externalApiInfo += `
+         <div class="project-card">
+         <div class="project-img-1">
+           <img
+             src="https://res.cloudinary.com/dfwqfes7e/image/upload/v1669374185/midterm/images/projects-section-20221121T165303Z-001/projects-section/${post.id}.jpg"
+             alt="cube-img"
+           />
+         </div>
+         <div class="project-content">
+           <h4>${post.title.slice(0, 10)}</h4>
+           <p>${post.body.slice(0, 30)}</p>
+           <a href="./index-project-${post.id}.html">Learn More</a>
+         </div>
+       </div>
+         `
+      })
+      document.querySelector("#collection-projects").innerHTML = externalApiInfo
+   })
+   .catch((error) => {console.log(error);})
+}
+
+getExternalApiInfo()
